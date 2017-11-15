da817be44fd4e4170b5bd6d0c1d7236ba903df6c
masuhara <info@masuhara.net>
2017-11-15 18:57:09 +0900

    Add Dynamic cell codes

--- MapCellSampler/ViewController.swift
+++ MapCellSampler/ViewController.swift
@@ -18,6 +18,9 @@ class ViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.mapTableView.estimatedRowHeight = 342
+        self.mapTableView.rowHeight = UITableViewAutomaticDimension
+        
         mapTableView.dataSource = self
         
         setSampleData()
@@ -38,18 +41,41 @@ class ViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MapCell") as! MapTableViewCell
         
+        // Setup Camera(Zoom)
         let zoom: Float = 15
-        
         let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude: sampleData[indexPath.row].latitude, longitude: sampleData[indexPath.row].longitude, zoom: zoom)
-        
         cell.mapView.camera = camera
         
+        // Add Pin
         let marker: GMSMarker = GMSMarker()
         marker.position = CLLocationCoordinate2DMake(sampleData[indexPath.row].latitude, sampleData[indexPath.row].longitude)
         marker.map = cell.mapView
         
+        // Other Contents
         cell.titleLabel.text = sampleData[indexPath.row].title
         cell.detailLabel.text = sampleData[indexPath.row].detail
+        
+        // Images
+        let length = cell.imageScrollView.bounds.height
+        if let images = sampleData[indexPath.row].images {
+            // cell.imageScrollViewHeightConstraint.constant = 100
+            cell.imageScrollView.alwaysBounceHorizontal = true
+            cell.imageScrollView.contentSize = CGSize(width: length * CGFloat(images.count), height: length)
+            
+            print(cell.imageScrollView.contentSize)
+            
+            for i in 0..<images.count {
+                let imageView = UIImageView(image: images[i])
+                imageView.frame = CGRect(x: (CGFloat(i) * length) + CGFloat(8 * i), y: 0, width: length, height: length)
+                imageView.contentMode = .scaleAspectFill
+                imageView.clipsToBounds = true
+                cell.imageScrollView.addSubview(imageView)
+            }
+            
+            print(cell.imageScrollView)
+        } else {
+            cell.imageScrollViewHeightConstraint.constant = 0
+        }
         
         return cell
     }
@@ -63,10 +89,14 @@ class ViewController: UIViewController, UITableViewDataSource {
         
         kanazawa.title = "金沢駅"
         kanazawa.detail = "金沢駅は、石川県金沢市木ノ新保町にある、西日本旅客鉄道・IRいしかわ鉄道の駅。"
+        kanazawa.images = [UIImage(named: "kanazawa01.jpg"), UIImage(named: "kanazawa02.jpg"), UIImage(named: "kanazawa03.jpg")]
+        
         hiroshima.title = "広島駅"
         hiroshima.detail = "広島駅は、広島県広島市南区松原町にある西日本旅客鉄道・広島電鉄の駅。"
+        
         shibuya.title = "渋谷駅"
         shibuya.detail = "渋谷駅は、東京都渋谷区道玄坂一丁目および同二丁目にある、東日本旅客鉄道・京王電鉄・東京急行電鉄・東京地下鉄の駅である。"
+        
         sapporo.title = "札幌駅"
         sapporo.detail = "札幌駅は、北海道札幌市北区北6条西4丁目にある、北海道旅客鉄道函館本線の駅。"
         
