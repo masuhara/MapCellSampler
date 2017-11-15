dc84e1a4b9be0c0af25ba83888d2e374c4dd7595
masuhara <info@masuhara.net>
2017-11-15 16:46:20 +0900

    Initial Commit

---
+++ MapCellSampler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapCellSampler
+//
+//  Created by Masuhara on 2017/11/15.
+//  Copyright © 2017年 Ylab, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
