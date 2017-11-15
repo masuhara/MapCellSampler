01edccb24f492ce9b515635d2520fe2c1fab621d
masuhara <info@masuhara.net>
2017-11-15 17:05:59 +0900

    Initial commit

---
+++ MapCellSampler/MapTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MapTableViewCell.swift
+//  MapCellSampler
+//
+//  Created by Masuhara on 2017/11/15.
+//  Copyright © 2017年 Ylab, Inc. All rights reserved.
+//
+
+import UIKit
+
+class MapTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
